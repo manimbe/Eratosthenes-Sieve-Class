bab0139984b2f3d8ef210236e3d8a2410b46585d
Manimbé <73669682+manimbe@users.noreply.github.com>
2021-03-21 15:56:26 -0300

    Added more features
    Added extra features: a method that gives you the amount of prime numbers in the established range, and other method that checks if a number (in the established range) is a prime number or not.
    
    Also, in the main function I show how to use those new methods.

--- src/main.cpp
+++ src/main.cpp
@@ -53,6 +53,31 @@ public:
 		std::cout << sum << std::endl;
 		
 	}	
+	
+//Amount function (display the amount of prime numbers)
+	void Amount(){
+		
+		i = 0;
+		unsigned int amount = 0;
+		
+		while(i < limit){
+			amount += prime[i];
+			i++;
+		}
+		
+		std::cout << amount << std::endl;
+		
+	}
+	
+//Check function (if the number is in between the established limits, checks if it is a prime number ("1") or not ("0"))
+	void Check(int p){
+		p -= 2;
+		int check;
+		prime[p] == 1 ? check = 1: check = 0;
+		
+		std::cout << check << std::endl;
+		
+	}
 
 //Destructor (deallocate heap memory)
 	~Sieve()
@@ -65,7 +90,9 @@ public:
 int main(){
 	
 Sieve a(10);
-a.Sum();
+a.Sum(); //what is the result of adding up each prime number (of the established range)
+a.Amount(); //how many prime numbers there are in between the established range (1-10 in this case)
+a.Check(7); //is "7" a prime number?
 	
 	return 0;
 }
