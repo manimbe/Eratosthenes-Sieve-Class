10ac7b2d7a7a444f93ae45d095b2f341e42f5ddf
Manimbé <73669682+manimbe@users.noreply.github.com>
2021-03-27 23:15:46 -0300

    Created main.cpp
    Created main.cpp to show the functionalities of the class (how it works).

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Sieve.h"
+
+//This file is just for seeing how the class works
+
+int main(){
+	
+Sieve example(10);
+example.Sum(); //what is the result of adding up each prime number? (of the established range)
+example.Amount(); //how many prime numbers there are in between the established range? (1-10 in this case)
+example.Check(7); //is "7" a prime number? ("7" must be in the established range)
+	
+	return 0;
+}
