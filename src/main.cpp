3ceccc6da7a9a70a49c6fe51fb0d6a09bb316e2b
Manimbé <73669682+manimbe@users.noreply.github.com>
2021-08-19 00:29:07 -0300

    Update main.cpp
    Just fixing a misspelling

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main(){
 	
 Sieve example(10);
-example.Sum(); //what is the result of adding up each prime number? (of the established range)
+example.Sum(); //what is the result of adding up each prime number? (from the established range)
 example.Amount(); //how many prime numbers there are in between the established range? (1-10 in this case)
 example.Check(7); //is "7" a prime number? ("7" must be in the established range)
 	
